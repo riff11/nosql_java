8245cf036000b3527df499767d5dc2cf3cc3b3ac
Sergiy <sergey.morenets@gmail.com>
2020-10-25 10:33:11 +0200

    Task 10.2

--- nosql-mongo/src/main/java/it/discovery/nosql/config/MongoDBConfiguration.java
+++ nosql-mongo/src/main/java/it/discovery/nosql/config/MongoDBConfiguration.java
@@ -1,10 +1,10 @@
 package it.discovery.nosql.config;
 
 import it.discovery.nosql.model.Book;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.index.Index;
@@ -12,13 +12,11 @@ import org.springframework.data.mongodb.core.index.Index;
 @Configuration
 public class MongoDBConfiguration {
 
-    @Autowired
-    private MongoOperations mongoOperations;
-
-    @EventListener
-    public void checkIndexes(ApplicationReadyEvent event) {
-        mongoOperations.indexOps(Book.class)
-                .ensureIndex(new Index().on("translations.text", Sort.Direction.ASC));
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> checkIndexes(MongoOperations mongoOperations) {
+        return event ->
+                mongoOperations.indexOps(Book.class)
+                        .ensureIndex(new Index().on("translations.text", Sort.Direction.ASC));
     }
 
 }
